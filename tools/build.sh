fe615108430a69960656fed23d8b0612d711cf6c
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-25 21:06:02 +0200

    refactor!: build tools
    - colors are now generated in ./colors
    - no vim color themes

--- tools/build.sh
+++ tools/build.sh
@@ -1,94 +1,99 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-setopt extendedglob
+#####################################################################
+#*******************************************************************#
+#* A simple script to:                                             *#
+#* - fetch the latest Base16 Schemes from the central repository   *#
+#*   - https://github.com/tinted-theming/base16-schemes            *#
+#* - convert the `yaml` to `lua`                                   *#
+#* - generate a list of all the builtin colorschemes               *#
+#* - run `stylua` on the generated lua files                       *#
+#*                                                                 *#
+#* Requirements:                                                   *#
+#* - stylua                                                        *#
+#* - bash                                                          *#
+#* - git                                                           *#
+#* - fd                                                            *#
+#*******************************************************************#
+# ###################################################################
 
-DIRNAME=${0:A:h}
-SCHEMES_SOURCE=https://github.com/base16-project/base16-schemes
-SCHEMES_DIR=${DIRNAME}/schemes
-LUA_DIR=${DIRNAME:h}/lua/colors
-VIM_DIR=${DIRNAME:h}/colors
+tools_dir="$(dirname "$0")"
+schemes_source="https://github.com/base16-project/base16-schemes"
+schemes_dir="$tools_dir/base16-schemes"
+colors_dir="$(dirname tools_dir)/colors"
+theme_list="$colors_dir/README.md"
 
-function get_schemes() {
-  if [ ! -d ${SCHEMES_DIR}/base16-schemes/.git ]
-  then
-    git clone -q --depth=1 ${SCHEMES_SOURCE} ${SCHEMES_DIR}/base16-schemes &
-  else
-    git \
-      --git-dir=${SCHEMES_DIR}/base16-schemes/.git \
-      --work-tree=${SCHEMES_DIR}/base16-schemes \
-      pull -q &
-  fi
-
-  printf "Cloning base16-schemes..."
-  wait
-  echo "Done"
+prepare() {
+	if [ ! -d "$colors_dir" ]; then
+		echo "Creating colors directory..."
+		mkdir -p "$colors_dir"
+	fi
+	if [ ! -f "$theme_list" ]; then
+		echo "Creating themes list..."
+		printf " # base16 colorschemes\n---\n" >"$theme_list"
+	fi
 }
 
-
-function process_vim() {
-  local color
-  local scheme=${1}
-  local i=1
-  echo "hi clear"
-  echo "let g:colors_name = 'base16-${scheme:t:r}'"
-  echo "lua require('base16-colorscheme').setup({"
-  for color in base0{0..9} base0{A..F}
-  do
-    [[ ${i} -eq 1 ]] && printf "    \ " || printf " "
-    local value=$(sed -ne 's/'"${color}"': "\(.*\)".*/\1/p' ${scheme})
-    printf "${color} = '#${value:l}'"
-    [[ ${color} != "base0F" ]] && printf ','
-    [[ ${i} -eq 4 ]] && echo && i=1 || ((i++))
-  done
-  echo "    \})"
+get_schemes() {
+	if [ ! -d "$schemes_dir/base16-schemes/" ]; then
+		echo "Cloning base16-schemes..."
+		git clone -q --depth=1 "$schemes_source" "$schemes_dir" &
+	else
+		echo "Pulling latest changes..."
+		git \
+			--git-dir="$schemes_dir" \
+			--work-tree="$schemes_dir" \
+			pull -q &
+	fi
+	wait
+	echo "Done"
 }
 
-function process_lua() {
-  local color
-  local scheme=${1}
-  local i=1
-  echo "return {"
-  for color in base0{0..9} base0{A..F}
-  do
-    [[ ${i} -eq 1 ]] && printf "    " || printf " "
-    local value=$(sed -ne 's/'"${color}"': "\(.*\)".*/\1/p' ${scheme})
-    printf "${color} = '#${value:l}'"
-    [[ ${color} != "base0F" ]] && printf ','
-    [[ ${i} -eq 4 ]] && echo && i=1 || ((i++))
-  done
-  echo "}"
+purge_colors() {
+	rm -rf "$colors_dir/*.lua"
+	sed -ne 's/^- base16-//p' "$theme_list"
 }
 
-function lua_init() {
-  local file name
-  printf "local M = {}\n\n"
-  for file in ${LUA_DIR}/*.lua~*/init.lua
-  do
-    name=${file:t:r}
-    echo "M['${name}'] = require('colors.${name}')"
-  done
-  printf "\nreturn M\n"
+update_list() {
+	color="$1"
+	echo "- base16-$color" >>"$theme_list"
 }
 
-function process() {
-  local name scheme
-  printf "Processing scheme files..."
-  rm -rf ${LUA_DIR}
-  mkdir -p ${LUA_DIR} ${VIM_DIR}
-  for scheme in ${SCHEMES_DIR}/*/*.yaml
-  do
-    name=${scheme:t:r}
-    process_lua ${scheme} > ${LUA_DIR}/${name}.lua &
-    process_vim ${scheme} > ${VIM_DIR}/base16-${name}.vim &
-  done
-  wait
-  lua_init > ${LUA_DIR}/init.lua
-  echo "Done"
+generate_lua() {
+	scheme="$1"
+	echo 'require("base16").setup({'
+	for color in base0{0..9} base0{A..F}; do
+		value=$(sed -ne 's/'"$color"': "\(.*\)".*/\1/p' "$scheme")
+		printf '\t%s = "%s",\n' "$color" "#$value"
+	done
+	echo "})"
 }
 
-function main() {
-  get_schemes
-  process
+loop() {
+	echo "Updating lua themes..."
+	mkdir -p "$colors_dir"
+	schemas="$(fd --glob '*.y*ml' --no-ignore | sort)"
+	for scheme in $schemas; do
+		fbname=$(basename "$scheme" | cut -d. -f1)
+		generate_lua "$scheme" >"$colors_dir/base16-$fbname.lua" &
+		update_list "$fbname"
+	done
+	wait
+	echo "Done!"
 }
 
-main
+cleanup() {
+	echo "Cleaning up..."
+	rm -rf "$schemes_dir"
+}
+
+run() {
+	prepare
+	purge_colors
+	get_schemes
+	loop
+	stylua .
+	cleanup
+}
+
+run
